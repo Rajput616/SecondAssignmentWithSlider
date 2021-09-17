a79d0d6b5aab93bee2f5f8466b839f698ed8088a
Rajput616 <saurabhrajput616@gmail.com>
2021-09-17 13:24:39 +0530

    Add project files.

---
+++ Second Assignment/Second Assignment/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Second_Assignment
+{
+    public partial class MainPage : ContentPage
+    {
+        static String[] str = { "Hey I love Coding", "It is fun", "Too much fun", "It is the best", "Hey Hey Hey", "Chirp Chirp" };
+        int i = 0;
+        int len = str.Length;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (i < len)
+            {
+                label.Text = str[i];
+                i++;
+
+                if (i == len)
+                {
+                    i = 0;
+                }
+            }
+        }
+
+
+        private void slider_DragCompleted(object sender, EventArgs e)
+        {
+            label.FontSize = (int)slider.Value;
+        }
+    }
+}
